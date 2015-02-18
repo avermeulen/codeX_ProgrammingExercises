9a2867fa4ecc185885bc291d176d37c84d866559
avermeulen <andre.vermeulen@gmail.com>
2015-02-18 13:32:35 +0200

    fixed qunit issues

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,9 @@ gulp.task('test', function() {
     return gulp.src('./ExerciseOne/test-words-utility.html')
         .pipe(qunit());
 });
+
+gulp.task('go', function(){
+
+	console.log("oi!");
+
+});
